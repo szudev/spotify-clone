17221f5a3fc622ad2ecaa8e2dde2eb0e85d2c59a
szudev <sz.ubiergo@gmail.com>
2023-12-05 17:16:09 -0300

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { getServerSession, NextAuthOptions } from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
 import spotifyApi, { LOGIN_URL } from './spotify'
 import { JWT } from 'next-auth/jwt'
+import { signOut } from 'next-auth/react'
 
 async function refreshAccessToken(token: JWT) {
   try {
@@ -17,7 +18,7 @@ async function refreshAccessToken(token: JWT) {
       refreshToken: refreshedToken.refresh_token ?? token.refreshToken
     }
   } catch (error) {
-    console.log({ HERE: error })
+    await signOut({ callbackUrl: '/login' })
     return {
       ...token,
       error: 'RefreshAccessTokenError'
