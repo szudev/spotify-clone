3cf0a12af5c117b42fbe8180d7540016fb47d04b
szudev <sz.ubiergo@gmail.com>
2023-12-22 20:21:08 -0300

    fix css issues

--- src/app/(main)/playlist/loading.tsx
+++ src/app/(main)/playlist/loading.tsx
@@ -5,7 +5,7 @@ import {
 
 export default function Loading() {
   return (
-    <div className='flex flex-col gap-6 pt-16 min-h-full flex-1 rounded-t-lg'>
+    <div className='flex flex-col gap-6 pt-16 flex-1 rounded-t-lg'>
       <PlaylistHeaderSkeleton />
       <div className='bg-gradient-to-b md:from-black/10 from-transparent md:to-zinc-900 to-zinc-900 to-[100px] md:to-[200px] w-full flex-1'>
         <PlaylistTableSkeleton />
