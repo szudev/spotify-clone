7a22ad505b5326a65c366faaf70e36f9b6e7408a
szudev <sz.ubiergo@gmail.com>
2023-12-22 15:28:14 -0300

    fix login page css issues

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginProviders } from '@/components/LoginProviders'
 export default async function Login() {
   return (
     <main className='md:from-zinc-900 md:to-black md:bg-gradient-to-b flex items-center justify-center flex-1 md:p-8 p-4 bg-black'>
-      <section className='rounded-lg bg-black md:py-32 py-8 text-center items-center text-white md:w-3/5 flex flex-col w-full'>
+      <section className='rounded-lg bg-black md:py-20 lg:py-28 py-0 text-center items-center text-white md:w-3/5 flex flex-col w-full'>
         <h1 className='sm:text-3xl md:text-4xl text-2xl font-bold mb-8'>
           Log in to Spotify Clone
         </h1>
